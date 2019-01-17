aec4d4cc39573580909292f4f5c49e62b194b69d
parentj <ParentJ@WSDOT.WA.GOV>
2019-01-17 11:54:45 -0800

    Add missing formatting

--- main.js
+++ main.js
@@ -1,37 +1,37 @@
 "use strict";
 
 //Initialize authorization token in a global context to be used in all requests.
-var authToken = ''
+var authToken = "";
 
 
 //Fetch list of available databases
 async function getDB() {
 
     //Setup dropdown stuff
-    let dropdown = document.getElementById('dbDropdown')
-    dropdown.length = 0
-    let defaultOption = document.createElement('option')
-    defaultOption.text = 'Select a Database'
-    dropdown.add(defaultOption)
-    dropdown.selectedIndex = 0
+    let dropdown = document.getElementById("dbDropdown");
+    dropdown.length = 0;
+    let defaultOption = document.createElement("option");
+    defaultOption.text = "Select a Database";
+    dropdown.add(defaultOption);
+    dropdown.selectedIndex = 0;
 
     //Setup Request URL
-    const dbURL = 'http://localhost:9000/api/auth/GetDatabases'
+    const dbURL = "http://localhost:9000/api/auth/GetDatabases";
 
     //Send request to get list of available databases and get back json data
     let response = await fetch(dbURL, {
-        method: 'GET'
-    })
-    let result = await response.json()
+        method: "GET"
+    });
+    let result = await response.json();
 
     // Set response data to create and populate a dropdown list on the page
-    let option
+    let option;
     for (let i = 0; i < result.length; i++) {
-        option = document.createElement('option')
-        option.text = 'db ID: ' + result[i].ID + ' ' + result[i].Name
+        option = document.createElement("option")
+        option.text = "db ID: " + result[i].ID + " " + result[i].Name
         option.value = result[i].ID
         dropdown.add(option)
-    }
+    };
 };
 
 
@@ -43,70 +43,70 @@ getDB();
 async function getAuth() {
 
     //Get database id value from page
-    let dbOption = document.getElementById('dbDropdown')
-    let dbValue = dbOption.options[dbOption.selectedIndex].value
+    let dbOption = document.getElementById("dbDropdown");
+    let dbValue = dbOption.options[dbOption.selectedIndex].value;
 
     //Alert if no database is selected
-    if (dbValue === '' | dbValue === 'Null' | dbValue === 'Select a Database') {
-        return alert('Please Select a Database First')
-    }
+    if (dbValue === "" | dbValue === "Null" | dbValue === "Select a Database") {
+        return alert("Please Select a Database First")
+    };
 
     //Setup request url and header info
-    const authURL = 'http://localhost:9000/api/auth/APILogin'
+    const authURL = "http://localhost:9000/api/auth/APILogin";
     let headers = {
-        Accept: 'application/JSON',
-        Authorization: 'Basic cG9udGlzOnBvbnRpcw==', //Base64 encoding of the string 'pontis:pontis'
+        Accept: "application/JSON",
+        Authorization: "Basic cG9udGlzOnBvbnRpcw==", //Base64 encoding of the string "pontis:pontis"
         database_id: "'" + dbValue + "'"
-    }
+    };
 
     //Set Get Request
     let response = await fetch(authURL, {
-        method: 'GET',
+        method: "GET",
         headers: headers
-    })
+    });
 
     //Process response from the request
-    let result = await response.json()
+    let result = await response.json();
 
     //Post response data to page
-    document.getElementById('authResponse').innerHTML = JSON.stringify(result, undefined, 2)
+    document.getElementById("authResponse").innerHTML = JSON.stringify(result, undefined, 2);
 
     //Set auth_token to global var
-    authToken = result.auth_token
+    authToken = result.auth_token;
 
     //Reset and start the 1200 sec timer
     /*TODO:  Need to add way to reset timer from here. 
     Issue when getting a new auth token and the timer doesnt reset.  
-    Currently when pressing the button before the timer ends it doesn't stop the function and reset the timer
+    Currently when pressing the button before the timer ends it doesn"t stop the function and reset the timer
     */
-    authTimer()
+    authTimer();
 
     //Pass auth token and get list of controllers available in the selected database
-    await getControllers(result.auth_token)
+    await getControllers(result.auth_token);
 
-    let hideGetNav = document.getElementById('hideGetDiv')
-    let hidePostDiv = document.getElementById('hidePostDiv')
-    let hidePutDiv = document.getElementById('hidePutDiv')
-    let hideDeleteDiv = document.getElementById('hideDeleteDiv')
-    hideGetNav.style.display = 'block'
-    hidePostDiv.style.display = 'block'
-    hidePutDiv.style.display = 'block'
-    hideDeleteDiv.style.display = 'block'
+    let hideGetNav = document.getElementById("hideGetDiv");
+    let hidePostDiv = document.getElementById("hidePostDiv");
+    let hidePutDiv = document.getElementById("hidePutDiv");
+    let hideDeleteDiv = document.getElementById("hideDeleteDiv");
+    hideGetNav.style.display = "block";
+    hidePostDiv.style.display = "block";
+    hidePutDiv.style.display = "block";
+    hideDeleteDiv.style.display = "block";
 };
 
 
 //Toggle Table Name Dropdown.  Display only if the "Dynamic" controller is selected and hide otherwise
-let controllerSelected = document.getElementById('getControllers')
+let controllerSelected = document.getElementById("getControllers");
 controllerSelected.onchange = function () {
-    let hiddenDiv = document.getElementById('tableDiv')
-    let clearTableName = document.getElementById('getTables')
-    if (controllerSelected.value === 'Dynamic') {
-        hiddenDiv.style.display = 'block'
+    let hiddenDiv = document.getElementById("tableDiv");
+    let clearTableName = document.getElementById("getTables");
+    if (controllerSelected.value === "Dynamic") {
+        hiddenDiv.style.display = "block"
     } else {
-        hiddenDiv.style.display = 'none'
+        hiddenDiv.style.display = "none"
         clearTableName.selectedIndex = 0
-    }
-}
+    };
+};
 
 
 //1200 second countdown timer to keep track of the authorization window.  Alert user and reset at end of timer. 
@@ -118,7 +118,7 @@ function authTimer() {
         document.getElementById("countdown").textContent = seconds
         if (seconds <= 0) {
             clearInterval(countdown)
-            alert('Authorization Token has expired.  Please request a new token')
+            alert("Authorization Token has expired.  Please request a new token")
             document.getElementById("countdown").textContent = 1200
             document.getElementById("authResponse").textContent = ""
         }
@@ -131,31 +131,31 @@ function authTimer() {
 async function getControllers(controllerAuthToken) {
 
     //Setup Request URL and headers
-    const controllerURL = 'http://localhost:9000/api/DataDict/getTables'
+    const controllerURL = "http://localhost:9000/api/DataDict/getTables";
     let headers = {
         auth_token: controllerAuthToken
-    }
+    };
 
     //Send GET Request
     let response = await fetch(controllerURL, {
-        method: 'GET',
+        method: "GET",
         headers: headers
-    })
+    });
 
     //Process response
-    let result = await response.text()
+    let result = await response.text();
 
     //Remove offending characters from the response and convert it to an actual array
-    let controllerArray = result.replace(/['"\[\]]+/g, '')
-    controllerArray = controllerArray.split(",")
+    let controllerArray = result.replace(/[""\[\]]+/g, "");
+    controllerArray = controllerArray.split(",");
 
     //Send response data to the tables datalist on the page
-    let datalist = document.getElementById('tablesList')
+    let datalist = document.getElementById("tablesList");
     for (let i = 0; i < controllerArray.length; i++) {
-        let option = document.createElement('option')
+        let option = document.createElement("option")
         option.value = controllerArray[i]
         datalist.appendChild(option)
-    }
+    };
 };
 
 
@@ -163,55 +163,55 @@ async function getControllers(controllerAuthToken) {
 async function getRequest() {
 
     //Get table and controller names
-    let tableName = document.getElementById('getTables').value
-    let controllerName = document.getElementById('getControllers').value
+    let tableName = document.getElementById("getTables").value;
+    let controllerName = document.getElementById("getControllers").value;
 
     //Build the middle of the URL string depending on if just a controller is selected, or the Dynamic controller is selected
     let restURL
 
-    if (controllerName === 'Dynamic') {
-        restURL = 'Dynamic/' + tableName + '/?'
+    if (controllerName === "Dynamic") {
+        restURL = "Dynamic/" + tableName + "/?"
     } else {
-        restURL = controllerName + '/?'
-    }
+        restURL = controllerName + "/?"
+    };
 
     //Get Input values from the form.  Will be used to build the rest of the GET Request URL
-    let inputs = document.getElementsByClassName('getInput')
-    let formValues = {}
+    let inputs = document.getElementsByClassName("getInput");
+    let formValues = {};
 
     //Parse the query options inputs and store in the formValues array
     //Will need to add some error handling here for incorrectly formatted input
     for (let i = 0; i < inputs.length; i++) {
         formValues[inputs[i].id] = inputs[i].value
-    }
+    };
 
     //Append the values from the formValues array
     for (let i in formValues) {
         if (formValues.hasOwnProperty(i)) {
-            if (formValues[i] === '') {
+            if (formValues[i] === "") {
                 continue;
             }
-            restURL += i + '=' + formValues[i] + '&'
+            restURL += i + "=" + formValues[i] + "&"
         }
-    }
+    };
 
-    restURL = restURL.slice(0, -1) //Needed to remove the last '&' from the string
+    restURL = restURL.slice(0, -1) //Needed to remove the last "&" from the string
 
     //Setup GET Request URL and header info
-    const getURL = 'http://localhost:9000/rest/'
+    const getURL = "http://localhost:9000/rest/";
     let headers = {
         auth_token: authToken
-    }
+    };
 
     //Send the GET request
     let response = await fetch(getURL + restURL, {
-        method: 'GET',
+        method: "GET",
         headers: headers
-    })
+    });
 
     //Process response
-    let result = await response.json()
+    let result = await response.json();
 
     //Post response data to page
-    document.getElementById('getResponse').innerHTML = JSON.stringify(result, undefined, 2)
+    document.getElementById("getResponse").innerHTML = JSON.stringify(result, undefined, 2);
 };
