d17f5edb4a3f366e30c1fea47fec0b2e9bc76f4f
parentj <ParentJ@WSDOT.WA.GOV>
2019-02-04 13:45:55 -0800

    Get list of databases

--- app.js
+++ app.js
@@ -5,7 +5,47 @@ Functions below will send a GET request to the AssetManagement
 Will then pass that data to a function for either a POST/PUT/DELETE request and send it to the BrM tables
 Will also send a POST request to a separate table on AssetManagement to record the */
 
+
+//Fetch list of available databases and populate the dropdown
+async function getDB() {
+
+    //Setup dropdown stuff
+    let dropdown = document.getElementById("dbDropdown");
+    dropdown.length = 0;
+    let defaultOption = document.createElement("option");
+    defaultOption.text = "Select a Database";
+    dropdown.add(defaultOption);
+    dropdown.selectedIndex = 0;
+
+    //Setup Request URL
+    const dbURL = "http://localhost:9000/api/auth/GetDatabases";
+
+    //Send request to get list of available databases and get back json data
+    let response = await fetch(dbURL, {
+        method: "GET"
+    });
+    let result = await response.json();
+
+    // Set response data to create and populate a dropdown list on the page
+    let option;
+    for (let i = 0; i < result.length; i++) {
+        option = document.createElement("option")
+        option.text = "db ID: " + result[i].ID + " " + result[i].Name
+        option.value = result[i].ID
+        dropdown.add(option)
+    };
+};
+
+//Get list of Databases on page load to be used for the Authorization Request
+getDB();
+
+
+//Send GET request for authorization token
 async function getAuth() {
+    //Get database id value
+    let dbOption = document.getElementById("dbDropdown");
+    let dbValue = dbOption.options[dbOption.selectedIndex].value;
+
     //Setup request url and header info
     const authURL = "http://localhost:9000/api/auth/APILogin";
     let headers = {
@@ -24,4 +64,5 @@ async function getAuth() {
     let result = await response.json();
     return result.auth_token
 };
+
 
